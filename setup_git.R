542d55f089b144db40614bdb2dd52089ebb5e0eb
Rita Han <ritahanbj@gmail.com>
2025-08-29 15:18:38 -0400

    Initial commit

---
+++ setup_git.R
@@ -0,0 +1,3 @@
+usethis::use_git() ## Choose the option to make the commit, then to restart RStudio
+## If you don't have git installed, you will likely benefit from reading
+## "Happy Git and GitHub for the useR" at https://happygitwithr.com/.
